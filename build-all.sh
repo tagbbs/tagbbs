ffe2df11ac25cc0390232c1f7ca7863230722662
Jianfei Wang <me@thinxer.com>
2014-02-14 17:01:08 +0800

    build: fix linking

--- build-all.sh
+++ build-all.sh
@@ -4,18 +4,21 @@ ROOT=`pwd`
 NAME=tagbbs
 RELEASE=`git describe --always --tag`
 
+echo Release: $RELEASE
+
 mkdir -p release
 cd release
 
 for os in windows linux darwin
 do
+    echo Building for $os...
     mkdir -p $NAME-$os-$RELEASE
     cd $NAME-$os-$RELEASE
     GOOS=$os go build github.com/thinxer/tagbbs/apibbsd
     GOOS=$os go build github.com/thinxer/tagbbs/sshbbsd
     cd ..
-    ln -s $NAME-$os-$RELEASE $NAME-$os
+    ln -sfvn $NAME-$os-$RELEASE $NAME-$os
 done
 
 cp -r $ROOT/webui tagbbs-webui-$RELEASE
-ln -s tagbbs-webui-$RELEASE tagbbs-webui
+ln -sf tagbbs-webui-$RELEASE tagbbs-webui
